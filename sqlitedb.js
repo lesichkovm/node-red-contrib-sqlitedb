5cb97a367cd85af7d537642144dd37ce1381c043
Sinevia Ltd <info@sinevia.com>
2019-04-13 08:21:43 +0100

    Added configuration and description

--- sqlitedb.js
+++ sqlitedb.js
@@ -1,11 +1,55 @@
-module.exports = function(RED) {
+const axios = require('axios')
+
+module.exports = function (RED) {
     function SqliteDbNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+        var apiurl = config.apiurl;
+        var apikey = config.apikey;
+        var sqlquery = config.sqlquery; // Type of query
+        var sql = config.sql;
+
+
+
+        node.on('input', function (msg) {
+            if (sqlquery == "msg.topic") {
+                if (typeof msg.topic !== 'string') {                    
+                    node.error("msg.topic : the query is not defined as string", msg);
+                    node.status({fill: "red", shape: "dot", text: "msg.topic error"});
+                    msg.payload = {status: "error", message: "msg.topic IS NOT a string"};
+                    node.send(msg);
+                    return false;
+                }
+                
+                if (msg.topic.length < 1) {                    
+                    node.error("msg.topic : the query is empty", msg);
+                    node.status({fill: "red", shape: "dot", text: "msg.topic error"});
+                    msg.payload = {status: "error", message: "msg.topic CAN NOT be empty string"};
+                    node.send(msg);
+                    return false;
+                }
+
+                sql = msg.topic;
+            }
+            axios({
+                method: 'post',
+                responseType: 'json',
+                url: apiurl,
+                data: {
+                    api_key: apikey,
+                    sql: sql
+                }
+            }).then(function (response) {
+                node.status({fill: "green", shape: "dot", text: "query ok"});
+                msg.payload = response.data;
+                node.send(msg);
+            }).catch(function (error) {
+                node.error("http : connection error", msg);
+                node.status({fill: "red", shape: "dot", text: "HTTP connection error"});
+                msg.payload = error;
+                node.send(msg);
+            });
         });
     }
-    RED.nodes.registerType("sqlitedb",SqliteDbNode);
+    RED.nodes.registerType("sqlitedb", SqliteDbNode);
 }
